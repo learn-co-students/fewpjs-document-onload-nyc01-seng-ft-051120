61050d57faf914ba607815df856e310480f9e0c0
smatalha <sma.talha@gmail.com>
2020-07-01 14:52:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",  () => {
+    update()
+});
+
+
+ const update = () => {
+    document.getElementById("text")
+        .innerHTML = "This is really cool!";
+}
